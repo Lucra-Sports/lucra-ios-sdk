5c863c89abf2169b1036f9822e72595f8bc43164
Michael Schmidt <mike.schmidt@lucrasports.com>
2023-09-13 11:29:13 -0400

    Updating to version 0.3.3

--- LucraSDK.podspec
+++ LucraSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = 'LucraSDK'
-    s.version          = '0.2.2'
+    s.version          = '0.3.0'
     s.summary          = 'LucraSDK for iOS'
 
     s.description      = <<-DESC
@@ -11,8 +11,10 @@ Pod::Spec.new do |s|
     s.license          = { :type => 'Copyright', :text => 'Copyright 2023 Lucra' }
     s.authors          = 'Lucra, Inc.'
 
-    s.module_name = "LucraSDK"
-    s.source = { :http => "https://lucra-sdk.s3.amazonaws.com/ios/cocoapods/#{s.version}/LucraSDK.zip" }
+    s.module_name      = "LucraSDK"
+    s.source         = { :http => "https://lucra-sdk.s3.amazonaws.com/ios/cocoapods/#{s.version}/LucraSDK.zip" }
+    # s.source           = { :http => 'file:' + __dir__ + '/LucraSDK.zip' }
+
     s.vendored_frameworks = "#{s.module_name}.xcframework", "MobileIntelligence.xcframework"
     s.preserve_paths = "#{s.module_name}.xcframework/*", "MobileIntelligence.xcframework/*"
 
--- SDK Sample/SDK Sample/SwiftUI Example/SwiftUIExample.swift
+++ SDK Sample/SDK Sample/SwiftUI Example/SwiftUIExample.swift
@@ -8,74 +8,11 @@
 import SwiftUI
 import LucraSDK
 
-private struct AlternateAppearance: Appearance {
-    func color(_ color: LucraColor) -> Color? {
-        switch color {
-        case .darkBlue:
-            return nil
-        case .primaryPurple:
-            return .darkBlue
-        case .lucraOrange:
-            return .blue
-        case .darkRed:
-            return .red
-        case .background:
-            return .darkBlue
-        case .darkBackground:
-            return .darkBlue
-        case .onBackground:
-            return .white
-        case .lightGray:
-            return nil
-        case .lightBlue:
-            return nil
-        case .brightPurple:
-            return .blue
-        case .darkPurple:
-            return .darkBlue
-        case .liveGameGreen:
-            return nil
-        case .brightOrange:
-            return .white
-        case .disabledGray:
-            return nil
-        case .lightPurple:
-            return .white
-        case .errorRed:
-            return nil
-        case .mediumPurple:
-            return .blue
-        case .paleOrange:
-            return .white
-        case .brightPink:
-            return .white
-        case .winningGreen:
-            return nil
-        case .losingPurple:
-            return nil
-        case .tiedBlue:
-            return nil
-        case .textColor:
-            return nil
-        @unknown default:
-            return nil
-        }
-    }
-    
-    func font(_ font: LucraFont) -> Font? {
-        switch font {
-        case .h1, .h2, .h3:
-            return Font.custom("AmericanTypewriter", fixedSize: 28)
-        default:
-            return nil
-        }
-    }
-}
-
 struct SwiftUIExample: View {
     @StateObject private var lucraClient = LucraClient(config: .init(environment: .init(authenticationClientID: lucraAPIKey,
                                                                                         environment: lucraEnvironment,
-                                                                                        urlScheme: lucraURLScheme)))
+                                                                                        urlScheme: lucraURLScheme),
+                                                                     appearance: ClientTheme()))
     @Environment(\.dismiss) private var dismiss
     @State private var currentLucraFlow: LucraFlow?
 
