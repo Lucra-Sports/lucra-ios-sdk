000f7970a2c9626fdba37436c192ad69d8357e45
Wellison Pereira <wellison@lucrasports.com>
2024-10-28 12:55:30 -0400

    SPM Checksum

--- Package.swift
+++ Package.swift
@@ -34,17 +34,17 @@ let package = Package(
         .binaryTarget(
             name: "LucraSDK",
             url: "\(hostedPackageURL)/LucraSDK.xcframework.zip",
-            checksum: "34cdccb3920daacc142887f40ad3814d55ae4d074080c579399df09964a59ac5"
+            checksum: "e7e0ead0ec057d353d23fd1fddc09f62f0c95cbf8ca00368c7057967b655aa9e"
         ),
         .binaryTarget(
             name: "MobileIntelligence",
             url: "\(hostedPackageURL)/MobileIntelligence.xcframework.zip",
-            checksum: "232bb3ef6def5cfd6cd0e910e17775d932f49557e677828beb9d595f0ca53fe8"
+            checksum: "6f7dc5c6a374ddda3f0ead3eadeecd499937f8449a065e06fe6398c55680eb7d"
         ),
         .binaryTarget(
             name: "GeoComplySDK",
             url: "\(hostedPackageURL)/GeoComplySDK.xcframework.zip",
-            checksum: "642f5e2a120fabf6f39fee4a079348bea743540ddd23a06d3f951a31fe55e17a"
+            checksum: "78bd38617aa73beccdbb282cd5daa3dc9385e896e36f6e5cf2347bd97139ad8e"
         )
     ]
 )
