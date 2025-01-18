e926fc5e6e57a4d15d451126a095b3fd62d0a3a2
Phillip English <vitkiarts@gmail.com>
2025-01-17 20:23:31 -0500

    Release/2.2.0 (#43)
    * Release 2.2.0
    
    * Update APIExample.swift

--- LucraSDK.podspec
+++ LucraSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = 'LucraSDK'
-    s.version          = '2.1.2'
+    s.version          = '2.2.0'
     s.summary          = 'LucraSDK for iOS'
 
     s.description      = <<-DESC
--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "2.1.2"
+let version = "2.2.0"
 
 let hostedPackageURL = "https://lucra-sdk.s3.amazonaws.com/ios/spm/\(version)"
 
@@ -34,17 +34,17 @@ let package = Package(
         .binaryTarget(
             name: "LucraSDK",
             url: "\(hostedPackageURL)/LucraSDK.xcframework.zip",
-            checksum: "20b673b9aa476f8216fb790d168fe18c87a75551e279c3b917a2464391e42325"
+            checksum: "d24784e8aeb69ad849f62c04a489c47a84043903907ef435b48e73d73b07b051"
         ),
         .binaryTarget(
             name: "MobileIntelligence",
             url: "\(hostedPackageURL)/MobileIntelligence.xcframework.zip",
-            checksum: "9127fa123d8eb9aecba15d95445d5d34bf1b14aaa99c87e0e81505cc70a93756"
+            checksum: "0c071994d63ec3ab2f48117bfd4eed6369d8428f54dc16631f96728aad324d03"
         ),
         .binaryTarget(
             name: "GeoComplySDK",
             url: "\(hostedPackageURL)/GeoComplySDK.xcframework.zip",
-            checksum: "f3cf5b626d42ab26e636730a0eea5b1a8c4364a1f4a0fa9ab2d499e5fb91d5c7"
+            checksum: "42f74759bfa0094851d0e863e8c8d9ef0aa8ae1e582f3c57b0e01d5d7a9f26a3"
         )
     ]
 )
