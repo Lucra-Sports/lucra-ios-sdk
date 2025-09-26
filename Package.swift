ed9c71a52a2d17dccf145375d59a66c13a62fd25
Michael Schmidt <mike.schmidt@lucrasports.com>
2025-09-26 17:41:52 -0400

    Bumping package file to 3.6.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "3.5.0"
+let version = "3.6.0"
 
 let hostedPackageURL = "https://lucra-sdk.s3.amazonaws.com/ios/spm/\(version)"
 
@@ -32,17 +32,17 @@ let package = Package(
         .binaryTarget(
             name: "LucraSDK",
             url: "\(hostedPackageURL)/LucraSDK.xcframework.zip",
-            checksum: "3880a41b36947eb22756a24802fa2972c5c19831c284724f1179ff572dc112f4"
+            checksum: "621fe85cf2693f5d6fe531fe91ec4bb95169171a60b6251d4b80624744436e0d"
         ),
         .binaryTarget(
             name: "MobileIntelligence",
             url: "\(hostedPackageURL)/MobileIntelligence.xcframework.zip",
-            checksum: "c0d0c7324c9aaf5407262c0efd8471c865da941a080b033ea93d4f79fa1a1cce"
+            checksum: "8254fef6235aa0591ef10e3584047dad3d0b5e8425a6c4c728a7be6b47f18b34"
         ),
         .binaryTarget(
             name: "GeoComplySDK",
             url: "\(hostedPackageURL)/GeoComplySDK.xcframework.zip",
-            checksum: "98de3e6c16ac3f91f441c0ee19fb1ed2ac12726755dec19f670944ecc977dd35"
+            checksum: "b5e7a1e8f196b45147b34584ecb302b3b4a6b6653b84284283ac7c9003de08c1"
         )
     ]
 )
