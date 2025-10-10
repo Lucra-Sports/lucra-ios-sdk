05897223dfcc8029ea4b3ec00d45309ffb18b1a7
Leandro Bartsch Thá <leandroxb1@hotmail.com>
2025-10-10 01:37:35 -0300

    version and checksums

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "3.6.1"
+let version = "3.6.2"
 
 let hostedPackageURL = "https://lucra-sdk.s3.amazonaws.com/ios/spm/\(version)"
 
@@ -32,17 +32,17 @@ let package = Package(
         .binaryTarget(
             name: "LucraSDK",
             url: "\(hostedPackageURL)/LucraSDK.xcframework.zip",
-            checksum: "eee920bd87ff1ceb6b5744d51e180035864c82e716f54d8bafbb6b136ee1fa17"
+            checksum: "c5c17d03dfb291ccb38073464302c3d449f0c7e2a7412984fbfa2910c5a042e6"
         ),
         .binaryTarget(
             name: "MobileIntelligence",
             url: "\(hostedPackageURL)/MobileIntelligence.xcframework.zip",
-            checksum: "9bad210cd719c1ebe070b610939783841737e71eccd9ac4d3fb919beef3b64f2"
+            checksum: "3ee83b395db5a756407587bf20a6d0094cf490c47c3c18079b9c3f85fc86bbf6"
         ),
         .binaryTarget(
             name: "GeoComplySDK",
             url: "\(hostedPackageURL)/GeoComplySDK.xcframework.zip",
-            checksum: "1c888f0fa1181fcf8158cda82228ea206918cc6e443d9357df75c134ed665d58"
+            checksum: "ffd90a107bda2c8fe0fdeffaabc0d1b21feb10c3b0bacb63a21c87a7b785e72b"
         )
     ]
 )
