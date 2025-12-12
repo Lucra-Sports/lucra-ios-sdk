a25e0eef59645834b0a2950216d1888cd65d2a33
Wellison Pereira <wellison@lucrasports.com>
2025-12-12 09:28:37 -0500

    SampleLogging

--- Example/Sources/Root/SampleLogging.swift
+++ Example/Sources/Root/SampleLogging.swift
@@ -24,20 +24,20 @@ class SampleLogging: BaseLogging {
         super.errorBreadcrumb(message, category: category)
     }
     
-//    override func error(_ message: String, category: String, file: String, function: String, line: Int) {
-//        super.error(message, category: category, file: file, function: function, line: line)
-//        logException(message: message,
-//                     file: file,
-//                     function: function,
-//                     line: line)
-//    }
+    override func error(_ message: String, category: String, file: String, function: String, line: Int, error: (any Error)? = nil) {
+        super.error(message, category: category, file: file, function: function, line: line)
+        logException(message: message,
+                     category: category,
+                     file: file,
+                     function: function,
+                     line: line)
+    }
     
     override func set(userId: String?) {
         super.set(userId: userId)
     }
     
-    private func logException(message: String, file: String, function: String, line: Int) {
-
+    private func logException(message: String, category: String, file: String, function: String, line: Int) {
         let params: [String: Any] = [ "exception_message": message,
                                       "file": file,
                                       "function": function,
